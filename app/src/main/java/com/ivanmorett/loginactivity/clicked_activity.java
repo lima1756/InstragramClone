38dccf6b226f05591a11052ff245990789d72f77
Luis Iván Morett Arévalo <ivanmorett@fb.com>
2018-06-28 13:24:29 -0700

    Desing

---
+++ app/src/main/java/com/ivanmorett/loginactivity/clicked_activity.java
@@ -0,0 +1,13 @@
+package com.ivanmorett.loginactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clicked_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicked_activity);
+    }
+}
